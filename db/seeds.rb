586a6ef26b307f95ab8616c03fd07f10fa78f110
leonard percival <leonardpercival@gmail.com>
2020-05-12 19:16:55 +0100

    livecode with full crud, bootstrap and fontawesome

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+pet1 = Pet.create(name: "snoopy", address: "America", species: "dog")
+pet2 = Pet.create(name: "pepper", address: "America", species: "cat")
